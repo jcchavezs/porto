e9b894583d861522d32ea1344d1b6e70a7c01fb8
José Carlos Chávez <jcchavezs@gmail.com>
2021-08-31 14:40:07 +0200

    chore: improves testdata

--- testdata/nopad/nopad.go
+++ testdata/nopad/nopad.go
@@ -1,6 +1,6 @@
 package nopad // import "github.com/jcchavezs/porto-integration/nopad"
 
-// GoldenPad returns empty string
-func GoldenPad(s string, length int) string {
+// NoPad returns the same string
+func NoPad(s string, length int) string {
 	return s
 }
