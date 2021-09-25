da83310ca75c594b58fcc5e4c50736a5ee252f9e
José Carlos Chávez <jcchavezs@gmail.com>
2021-09-25 14:26:09 +0200

    chore: improves output messages and code readability.

--- cmd/porto/main.go
+++ cmd/porto/main.go
@@ -13,15 +13,15 @@ import (
 )
 
 func main() {
-	flagWrite := flag.Bool("w", false, "write result to (source) file instead of stdout")
-	flagList := flag.Bool("l", false, "list files whose vanity import differs from porto's")
+	flagWriteOutputToFile := flag.Bool("w", false, "write result to (source) file instead of stdout")
+	flagListDiff := flag.Bool("l", false, "list files whose vanity import differs from porto's")
 	flagSkipFiles := flag.String("skip-files", "", "Regexps of files to skip")
 	flag.Parse()
 
 	baseDir := flag.Arg(0)
 	if len(flag.Args()) == 0 {
 		fmt.Println(`
-usage: porto [options] path
+usage: porto [options] <target-path>
 
 Options:
 -w             Write result to (source) file instead of stdout (default: false)
@@ -31,19 +31,19 @@ Options:
 Examples:
 
 Add import path to a folder
-    $ porto -w myproject
+    $ porto -w ./myproject
 		`)
 		os.Exit(0)
 	}
 
 	baseAbsDir, err := filepath.Abs(baseDir)
 	if err != nil {
-		log.Fatalf("failed to resolve base absolute path for %q: %v", baseDir, err)
+		log.Fatalf("failed to resolve base absolute path for target path %q: %v", baseDir, err)
 	}
 
 	workingDir, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("failed to resolve base absolute path for %q: %v", baseDir, err)
+		log.Fatalf("failed to resolve base absolute path for current working dir: %v", err)
 	}
 
 	var skipFilesRegex []*regexp.Regexp
@@ -57,16 +57,16 @@ Add import path to a folder
 		}
 	}
 
-	c, err := porto.FindAndAddVanityImportForDir(workingDir, baseAbsDir, porto.Options{
-		WriteResultToFile: *flagWrite,
-		ListDiffFiles:     *flagList,
+	diffCount, err := porto.FindAndAddVanityImportForDir(workingDir, baseAbsDir, porto.Options{
+		WriteResultToFile: *flagWriteOutputToFile,
+		ListDiffFiles:     *flagListDiff,
 		SkipFilesRegexes:  skipFilesRegex,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if *flagList && c > 0 {
+	if *flagListDiff && diffCount > 0 {
 		os.Exit(2)
 	}
 }
